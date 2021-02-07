c3af49b4eb0f3a9a85b8ceefc07f380869341a38
amin <amin.saied@gmail.com>
2021-02-06 21:28:47 -0800

    add .vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,6 +48,8 @@ ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
 # is .vimrc already there then copy it to dotfiles/backup
 [ -f ~/.vimrc ] && cp -f ~/.vimrc ~/dotfiles/backups/.vimrc && rm -r ~/.vimrc
 ln -s ${BASEDIR}/.vimrc ~/.vimrc
+[ -f ~/.vim ] && cp -f ~/.vim ~/dotfiles/backups/.vim && rm -r ~/.vim
+ln -s ${BASEDIR}/.vim ~/.vim
 # you-complete-me plugin
 sudo apt install build-essential cmake
 cd ~/.vim/plugged/YouCompleteMe
