2ea6d81aa595f8fa7d68c99d882ed87d83c62ef1
amin <amin.saied@gmail.com>
2021-02-06 20:57:09 -0800

    install zsh and oh my zsh, backup dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,28 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+##########################
+## PACKAGE INSTALLATION ##
+##########################
+
+# install ZSH
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+rm -r install.sh
+
+# install oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#######################
+## SYM-LINK DOTFILES ##
+#######################
+
+# backup any existing dotfiles
+mkdir ~/dotfiles/backups
+
 # bash
+# is .bashrc already there then copy it to dotfiles/backup
+[ -f ~/.bashrc ] && cp -f ~/.bashrc ~/dotfiles/backups/.bashrc && rm -r ~/.bashrc
 ln -s ${BASEDIR}/.bashrc ~/.bashrc
 
 # zsh
@@ -12,10 +33,18 @@ ln -s ${BASEDIR}/.zshrc ~/.zshrc
 ln -s  ${BASEDIR}/.oh-my-zsh ~/.oh-my-zsh
 
 # git
-ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
+# if .gitconfig already there then leave it alone
+! [ -f ~/.gitconfig ] && ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
 
 # tmux
 ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
 
 # vim
+# is .vimrc already there then copy it to dotfiles/backup
+[ -f ~/.vimrc ] && cp -f ~/.vimrc ~/dotfiles/backups/.vimrc && rm -r ~/.vimrc
 ln -s ${BASEDIR}/.vimrc ~/.vimrc
+
+# this will answer yes to any installation questions
+# https://linux.die.net/man/1/yes
+yes
+EOF
